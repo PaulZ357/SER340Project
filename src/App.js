63a609b350e817bb6b338cae7203359c1f82ddf3
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-12-04 00:47:21 -0500

    courses screen, need fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Home from "./pages/home.jsx"
+import SelectCourse from './pages/selectcourse.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from './login.jsx'
+import Table from "./table.jsx"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
+    /*<div className="App">
+      <SelectCourse/>
+    </div>*/
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Table />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
